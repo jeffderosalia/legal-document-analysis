bda68a7db744372265a6ee86e498ed65b8399970
Jeff Derosalia <jeffderosalia@Jeffs-MacBook-Pro-2.local>
2024-12-17 18:04:00 -0500

    env var fix, take 1

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {  
+  const env = loadEnv(mode, process.cwd(), 'VITE_');
   return {
     plugins: [react()],
     define: {
-      'process.env': process.env,
-      "process.env.NODE_ENV": JSON.stringify(mode),
-    },
+      'process.env.VITE_ANTHROPIC_API_KEY': JSON.stringify(env.VITE_ANTHROPIC_API_KEY),
+      'process.env.VITE_FOUNDRY_API_URL': JSON.stringify(env.VITE_FOUNDRY_API_URL),
+      'process.env.VITE_FOUNDRY_CLIENT_ID': JSON.stringify(env.VITE_FOUNDRY_CLIENT_ID),
+      'process.env.VITE_FOUNDRY_REDIRECT_URL': JSON.stringify(env.VITE_FOUNDRY_REDIRECT_URL),
+      'process.env.VITE_OPENAI_API_KEY': JSON.stringify(env.VITE_OPENAI_API_KEY),
+     },
   };
 });
